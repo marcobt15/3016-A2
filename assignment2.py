a540e5417568d51d00453a20a2ffec36f3c97971
marcobt15 <mtoito03@gmail.com>
2024-10-17 15:02:56 -0400

    add helpers

--- assignment2.py
+++ assignment2.py
@@ -1,24 +1,39 @@
 import pandas as pd
+import statistics as stats
+
 def read_data(filepath):
   df = pd.read_csv(filepath, header=None)
-  print(df)
+  # print(df)
 
-  healthy_set = set()
-  diseased_set = set()
+  healthy_temp = []
+  healthy_heart_rate = []
+  diseased_temp = []
+  diseased_heart_rate = []
 
-  for row in df:
+  for _, row in df.iterrows():
     if row[0] == "healthy":
-      healthy_set.add(row[1:3])
+      healthy_temp.append(row[1])
+      healthy_heart_rate.append(row[2])
     else:
-      diseased_set.add(row[1:3])
+      diseased_temp.append(row[1])
+      diseased_heart_rate.append(row[2])
 
-  return healthy_set, diseased_set
+  return healthy_temp, healthy_heart_rate, diseased_temp, diseased_heart_rate
+
+def get_stats(curr_list):
+  return stats.mean(curr_list), stats.stdev(curr_list)
+
+
 
 def naive_bayes_classifier(dataset_filepath, patient_measurements):
   # dataset_filepath is the full file path to a CSV file containing the dataset
   # patient_measurements is a list of [temperature, heart rate] measurements for a patient
-  healthy_set, deseased_set = read_data(dataset_filepath)
-  
+  healthy_temp, healthy_heart_rate, diseased_temp, diseased_heart_rate = read_data(dataset_filepath)
+
+  healthy_temp_mean, healthy_temp_stddev = get_stats(healthy_temp)
+  healthy_hr_mean, healthy_hr_stddev = get_stats(healthy_heart_rate)
+  diseased_temp_mean, diseased_temp_stddev = get_stats(diseased_temp)
+  diseased_hr_mean, diseased_hr_stddev = get_stats(diseased_heart_rate)
   
   most_likely_class = "none"
   class_probabilities = [0,0]
