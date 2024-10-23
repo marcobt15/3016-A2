5d7ac36c4bbe5221e6003303f15ab8127d926435
marcobt15 <mtoito03@gmail.com>
2024-10-22 20:30:01 -0400

    final commit, cleaning up code and comments

--- assignment2.py
+++ assignment2.py
@@ -1,10 +1,9 @@
 import pandas as pd
-import statistics as stats
-import math 
+import statistics as stats #from standard library
+import math #from standard library
 
 def read_data(filepath):
   df = pd.read_csv(filepath, header=None)
-  # print(df)
 
   healthy_temp = []
   healthy_heart_rate = []
@@ -23,10 +22,9 @@ def read_data(filepath):
       diseased_heart_rate.append(row[2])
 
   total_patients = df.shape[0]
-  #print('total_patients', total_patients)
+
   diseased_count = total_patients - healthy_count
-  # print('healthy_patients', healthy_count)
-  # print('diseased_patients', diseased_count)
+
   return healthy_temp, healthy_heart_rate, diseased_temp, diseased_heart_rate, healthy_count, diseased_count, total_patients
 
 def get_stats(curr_list):
@@ -44,31 +42,11 @@ def naive_bayes_classifier(dataset_filepath, patient_measurements):
   # patient_measurements is a list of [temperature, heart rate] measurements for a patient
   healthy_temp, healthy_heart_rate, diseased_temp, diseased_heart_rate, healthy_count, diseased_count, total_patients = read_data(dataset_filepath)
 
-  # print("healthy_temp", healthy_temp)
-  # print("healthy_heart_rate", healthy_heart_rate)
-  # print("diseased_temp", diseased_temp)
-  # print("diseased_heart_rate", diseased_heart_rate)
-  # print("healthy_count", healthy_count)
-  # print("diseased_count", diseased_count)
-  # print("total_patients", total_patients)
-
   #get all means and standard deviations for every feature and class
   healthy_temp_mean, healthy_temp_stddev = get_stats(healthy_temp)
   healthy_hr_mean, healthy_hr_stddev = get_stats(healthy_heart_rate)
   diseased_temp_mean, diseased_temp_stddev = get_stats(diseased_temp)
   diseased_hr_mean, diseased_hr_stddev = get_stats(diseased_heart_rate)
-
-  # print("healthy_temp_mean", healthy_temp_mean)
-  # print("healthy_temp_stddev", healthy_temp_stddev)
-
-  # print("healthy_hr_mean", healthy_hr_mean)
-  # print("healthy_hr_stddev", healthy_hr_stddev)
-
-  # print("\ndiseased_temp_mean", diseased_temp_mean)
-  # print("diseased_temp_stddev", diseased_temp_stddev)
-
-  # print("diseased_hr_mean", diseased_hr_mean)
-  # print("diseased_hr_stddev", diseased_hr_stddev)
 
   patient_temp = patient_measurements[0]
   patient_hr = patient_measurements[1]
@@ -82,39 +60,26 @@ def naive_bayes_classifier(dataset_filepath, patient_measurements):
   #P(hr|diseased)
   p_of_hr_given_diseased = probability_of_feature_given_class(diseased_hr_mean, diseased_hr_stddev, patient_hr)
 
-  # print("\nP(temp|healthy)", p_of_temp_given_healthy)
-  # print("P(hr|healthy)", p_of_hr_given_healthy)
-  # print("P(temp|diseased)", p_of_temp_given_diseased)
-  # print("P(hr|diseased)", p_of_hr_given_diseased)
-
   # P(healthy)
   p_of_healthy = healthy_count / total_patients
-  #print("\nP(healthy)", p_of_healthy)
 
   # P(diseased)
   p_of_diseased = diseased_count / total_patients
-  #print("P(diseased)", p_of_diseased)
 
   # P(hr = x | healthy) * P(temp = y | healthy) * P(healthy)
   p_of_hr_and_temp_healthy = p_of_hr_given_healthy * p_of_temp_given_healthy * p_of_healthy
-  #print("P(hr and temp | healthy)", p_of_hr_and_temp_healthy)
 
   # P(hr = x | diseased) * P(temp = y | diseased) * P(diseased)
   p_of_hr_and_temp_diseased = p_of_hr_given_diseased * p_of_temp_given_diseased * p_of_diseased
-  #print("P(hr and temp | diseased)", p_of_hr_and_temp_diseased)
 
   # P(hr = x , temp = y) = P(hr = x | healthy) * P(temp = y | healthy) * P(healthy) + P(hr = x | diseased) * P(temp = y | diseased) * P(diseased)
   p_of_hr_and_temp = p_of_hr_and_temp_healthy + p_of_hr_and_temp_diseased
-  #print("P(hr and temp)", p_of_hr_and_temp)
 
   # P(diseased | hr = x , temp = y) = P(hr = x | diseased) * P(temp = y | diseased) * P(diseased) / P(hr = x , temp = y)
   p_of_diseased_given_hr_and_temp = p_of_hr_and_temp_diseased / p_of_hr_and_temp
-  #print("P(diseased | hr and temp)", p_of_diseased_given_hr_and_temp)
 
   # P(healthy | hr = x, temp = y) = P(hr = x | healthy) * P(temp = y | healthy) * P(healthy) / P(hr = x , temp = y)
   p_of_healthy_given_hr_and_temp = p_of_hr_and_temp_healthy / p_of_hr_and_temp
-  #print("P(healthy | hr and temp)", p_of_healthy_given_hr_and_temp)
-
 
   if p_of_diseased_given_hr_and_temp > p_of_healthy_given_hr_and_temp:
     most_likely_class = "diseased"
@@ -126,17 +91,3 @@ def naive_bayes_classifier(dataset_filepath, patient_measurements):
   # most_likely_class is a string indicating the most likely class, either "healthy", "diseased"
   # class_probabilities is a two element list indicating the probability of each class in the order [healthy probability, diseased probability]
   return most_likely_class, class_probabilities
-
-example = '0'
-
-filepath_dataset = 'Examples\\Example'+example+'\\dataset.csv'
-filepath_measurements = 'Examples\\Example'+example+'\\patient_measurements.txt'
-
-with open(filepath_measurements, 'r') as file:
-  measurements = file.read().strip()
-  measurements = measurements.replace('[', '').replace(']', '').split(',')
-  measurements = [float(measurements[0].strip()), int(measurements[1].strip())]
-
-# print(measurements)
-
-print(naive_bayes_classifier(filepath_dataset, measurements))
